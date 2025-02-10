5b4dee52e704f3512aed3e300bc2147ce4095505
Caliber619 <caliber619pubg@gmail.com>
2025-02-11 01:02:10 +0530

    creating own react

--- 01react/src/App.jsx
+++ 01react/src/App.jsx
@@ -1,12 +1,16 @@
 import Cali from "./cali"
+
 function App() {
+
+  const username = "MERLIN"
   return (
     // <></> is called a fragment because ek hi element return krskte h
     <>
-      <h1>React with vite | Kshitij Dev</h1>
+      <h1>React with vite | Kshitij Dev aka {username}</h1>  {/* username -> evaluated expression hi likhte hai baaki ispe saare function condition kahin aur lagao idhar bhai final expression likho {iske andar} */}
       <Cali/>
     </>
   )
+
 }
 
 export default App
--- 01react/src/main.jsx
+++ 01react/src/main.jsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  )
+}
+const anotherUser = "Merlin"   /*//evaluated expression smjhne ke liye*/
+const reactElement  = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'click me to visit google',
+  anotherUser
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // reactElement
+  <App />
 )
