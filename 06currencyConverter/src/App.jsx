e4b32476eceab03078fca690704bb98d1da817e4
Caliber619 <caliber619pubg@gmail.com>
2025-02-21 10:07:30 +0530

    currency converter started

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   <>
+    <h1 className='text-center text-3xl bg-teal-300'>Currency App</h1>
+   </>
+  )
+}
+
+export default App
