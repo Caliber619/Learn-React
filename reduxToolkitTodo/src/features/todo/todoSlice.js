e9bd32fcc261eeac5292be0a32ca336f555626c8
Caliber619 <caliber619pubg@gmail.com>
2025-02-26 22:00:19 +0530

    updates

--- reduxToolkitTodo/src/features/todo/todoSlice.js
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+//store starting me kesa dikhega
 const initialState = {
     todos: [
         {
@@ -18,11 +19,27 @@ export const todoSlice = createSlice({
     //slices ke naam hote hain! and initial state hota hai
     name: 'todo',
     initialState,
+     
     // idhar hi reducers bante hain! - reducer me property and function hota hai
     reducers: {
+        // addTodo: sayHello     or 
+        
+        //redux toolkit me hum sirf declare hi nahi balki define bhi krte hain
         // yaha humesha do cheeze milegi - state, action
-        addTodo: (state, action)=> {},
-        removeTodo: ()=>{}, 
+        addTodo: (state, action)=> {
+            const todo = {
+                id: nanoid(),
+                text: action.payload
+                // payload ek object hai
+            }
+            // ab is todo ko state me bhejna hai
+            state.todos.push(todo)
+        },
+        removeTodo: (state, action)=>{
+            state.todos = state.todos.filter((todo)=>todo.id !== action.payload)
+        }, 
     }
 })
 
+export const {addTodo, removeTodo} = todoSlice.actions
+export default todoSlice.reducer
