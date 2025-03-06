4246424acdf5c00157d97d4e8bc295641f4420dd
Caliber619 <caliber619pubg@gmail.com>
2025-03-06 11:09:00 +0530

    Build an RTE (Real Time Editor)

--- 12BlogApp/src/components/index.js
+++ 12BlogApp/src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container"
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 
 export {
     Header,
@@ -10,5 +11,5 @@ export {
     Container,
     LogoutBtn,
     Logo,
-
+    RTE,
 }
